5afca0f5d90bba22c1146e216ae3c2e95d86121d
Boyu Zhou <bzhou7@huskers.unl.edu>
2025-08-28 21:55:54 +0800

    added my major to the output

--- hello.c
+++ hello.c
@@ -4,3 +4,13 @@
  *
  * A simple program my name and my major
  */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+    printf("Boyu ZHou!!!\n");
+    printf("Electrical and Electroinc Engineering\n");
+
+    return 0;
+}
